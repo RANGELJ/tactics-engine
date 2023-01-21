9992279084f5bad2e6275dc8e797e61b3e394dd9
Jorge Rangel <jorgerangel@192.168.1.3>
2023-01-21 16:05:46 -0600

    Enable cull and depth test

--- src/shared/glSceneDrawWithProgram.ts
+++ src/shared/glSceneDrawWithProgram.ts
@@ -16,6 +16,10 @@ const glSceneDrawWithProgram = ({
 
     gl.clearColor(0, 0, 0, 0)
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+
+    gl.enable(gl.CULL_FACE)
+    gl.enable(gl.DEPTH_TEST)
 
     useProgram()
 
